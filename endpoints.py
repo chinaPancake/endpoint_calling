6b4c3dee8d0130b1493f03c2449f2a10892ecce1
ChinaPancake <mateusz.placek99@gmail.com>
2023-02-18 15:23:42 +0100

    'first'

---
+++ endpoints.py
@@ -0,0 +1,4 @@
+url = "https://gate.dataloop.ai/api/v1"
+
+drivers = f"{url}/drivers"
+
---
+++ main.py
@@ -0,0 +1,14 @@
+import requests
+import endpoints
+
+def get_function():
+    response = requests.get(endpoints.drivers)
+    if response.status_code == 200:
+        data = response.json()
+        print(f'success, You get data from {endpoints.drivers}')
+    else:
+        print(f'Status code fail with: {response.status_code}')
+
+get_function()
+
+
