7f7ff6a60064c5bcbe181cc04fc992e070b4105f
ChinaPancake <mateusz.placek99@gmail.com>
2023-02-18 16:02:34 +0100

    fix

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ def get_pet(id:str):
     if response.status_code == 200:
         data = response.json()
         print(f'success, You get data from {endpoints.drivers}')
+        print(data)
     else:
         print(f'Status code fail with: {response.status_code}')
 
